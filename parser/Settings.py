10de240802d775b15c77e38ba45eb277aafdf64b
TheRootOf3 <andrzej.szablewski.20@ucl.ac.uk>
2021-03-19 00:19:05 +0100

    Added parser/analyzer settings

---
+++ parser/Settings.py
@@ -0,0 +1,6 @@
+DEBUG = False
+
+fileFrom = "parser/res/input/test_file.py"
+fileTo = "parser/res/output/test_file_edited.py"
+pathFrom = "./parser/res/input/test_file.py"
+pathTo = "./parser/res/output/test_file_edited.py"
