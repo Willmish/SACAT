f6d808b2e517eaf7f74b8ced64ac691d4a0d07e0
TheRootOf3 <andrzej.szablewski.20@ucl.ac.uk>
2021-03-23 00:22:02 +0100

    Changed line endings

--- parser/res/output/test_file_edited.py
+++ parser/res/output/test_file_edited.py
@@ -1,9 +1,18 @@
-lines_dict = {n : 0 for n in [2, 3, 4]}
+lines_dict = {n : 0 for n in [5, 6, 7, 8, 9, 10]}
+
+# hahahaha
+
+
+lines_dict[5] += 1
 def mySort(arr):
-    lines_dict[2] += 1
+    lines_dict[6] += 1
+    for x in range(5):
+        lines_dict[7] += 1
+        a = 1
+    lines_dict[8] += 1
     a = 1
-    lines_dict[3] += 1
+    lines_dict[9] += 1
     b = a + 1
-    lines_dict[4] += 1
+    lines_dict[10] += 1
     return b
 
