b188d61519ac02454aa0a5ea62b9ffbcbf50082a
tim-w1d <tim.widmayer.20@ucl.ac.uk>
2021-03-23 13:39:42 +0000

    add option to select type of test to run.

--- testing/run_environment.py
+++ testing/run_environment.py
@@ -25,12 +25,12 @@ class RunEnvironment():
     self.lst_size = 1
     self.lst_gen = ListGenerator(size = self.lst_size)
     self.test_sizes = []
-    self.test_output = [] 
+    self.test_output = []
 
   def signal_handler(signum, frame):
     raise Exception("Timed out!")
 
-  def run(self, my_sort):
+  def run_signal(self, my_sort):
     signal.signal(signal.SIGALRM, signal_handler)
     signal.alarm(10)   # Ten seconds
     try:
@@ -38,19 +38,38 @@ class RunEnvironment():
     except Exception, msg:
         print "Timed out!"
   
-  def run_random(self, my_sort):
+  def run(self, my_sort, time_analysis:bool, operations_analysis:bool, space_analysis:bool, random:bool, duplicate:bool, sortedd:bool, reversedd:bool):
+    storage_lst = []
+    if random:
+      storage_lst.append(run_with_generator(my_sort, self.lst_gen.random_lst, "random", time_analysis, operations_analysis, space_analysis))
+    if duplicate:
+      storage_lst.append(run_with_generator(my_sort, self.lst_gen.duplicate_lst, "duplicate", time_analysis, operations_analysis, space_analysis))
+    if sortedd:
+      storage_lst.append(run_with_generator(my_sort, self.lst_gen.sorted_lst, "sorted", time_analysis, operations_analysis, space_analysis))
+    if reversedd:
+      storage_lst.append(run_with_generator(my_sort, self.lst_gen.reversed_lst, "reversed", time_analysis, operations_analysis, space_analysis))
+    return storage_lst
+
+  def run_with_generator(self, my_sort, generator, test_type, time_analysis:bool, operations_analysis:bool, space_analysis:bool):
     test_count = 0
+    storage = TestStorage(test_type)
     while(test_count < self.max_tests):
-      lst_gen.size = self.lst_size
-      lst_gen.random_lst()
-      
+      self.lst_gen.size = self.lst_size
+      lst = generator()
+      size = len(lst)
+      time, operations, space = None, None, None
+      if time_analysis:
+        time = run_test_time(my_sort, lst)
+      if operations_analysis:
+        operations = run_test_operations(my_sort, lst)
+      if space_analysis:
+        operations = run_test_space(my_sort, lst)
+      storage.append(size, time, operations, space)
       #TODO is this the right way to increase the size?
       self.lst_size += self.step
       test_count += 1
-
-
-  def run_all_tests(self, my_sort, lst):
-    return run_test_time(), run_test_operations(), run_test_space()
+    self.lst_size = 1
+    return storage
 
   def run_test_time(self, my_sort, lst):
     pass
@@ -60,3 +79,4 @@ class RunEnvironment():
 
   def run_test_space(self, my_sort, lst):
     pass
+
