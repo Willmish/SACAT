3402279fab00de4937fc3ed74f3c57dabcc3d344
TheRootOf3 <andrzej.szablewski.20@ucl.ac.uk>
2021-04-06 21:28:09 +0200

    Added temp folder

--- sacat_project/settings.py
+++ sacat_project/settings.py
@@ -3,8 +3,8 @@ sys.setrecursionlimit(10**6)
 
 DEBUG = False
 
-fileFrom = "./res/tmp/.sort.py"
-fileTo = "./res/tmp/.sort_edited.py"
+fileFrom = "./res/temp/.sort.py"
+fileTo = "./res/temp/.sort_edited.py"
 
 safety_restricted_funcs = ["compile", "dir", "eval", "exec", "globals", 'input', 'open']
 other_restricted_funcs = ["enumerate", "filter", 'iter', 'map', 'next', 'sorted', 'zip']
